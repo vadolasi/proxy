dde7e9017f2f828b69f289f086f2198fff783ac1
Vitor Daniel <vitor036daniel@gmail.com>
2023-01-20 23:10:29 -0300

    initial commit

---
+++ index.ts
@@ -0,0 +1,24 @@
+import express from "express"
+import { createProxyMiddleware } from "http-proxy-middleware"
+
+const app = express()
+
+app.use("/", (req, res, next) => {
+  Object.keys(req.query).forEach(key => {
+    if (key.startsWith("h-")) {
+      res.setHeader(key.replace("h-", ""), req.query[key] as string)
+    }
+  })
+
+  createProxyMiddleware({
+    target: req.query.url as string,
+    changeOrigin: true,
+    pathRewrite: {
+      [`^/`]: ""
+    }
+  })(req, res, next)
+})
+
+app.listen(8000, () => {
+  console.log("Server started on port 8000")
+})
